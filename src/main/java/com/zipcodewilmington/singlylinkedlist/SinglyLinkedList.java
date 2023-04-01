97efef4ef08bb3dbbab54ec5a41ee96dc3d5342e
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-04-01 14:15:19 -0400

    added Node class and add method

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,33 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    class Node<T>{
+        Node<T> next;
+        T value;
+        public Node(T value){
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    Node<T> head;
+    Node<T> tail;
+
+    public SinglyLinkedList(){
+        head = null;
+        tail = null;
+    }
+
+    public void add(Node<T> node){
+        if(this.head==null){
+            head = node;
+        } else {
+            Node<T> temp = head;
+            while(temp.next!=null){
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
 }
