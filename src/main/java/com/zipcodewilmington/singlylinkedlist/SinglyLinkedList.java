e07a5dea69498964db023a5dae956d8df2d4440c
Nguyet <nguyet@zipcodersmbp103.mynetworksettings.com>
2023-04-01 17:58:04 -0400

    added copy method

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,7 +4,7 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList<T> {
-    class Node<T>{
+    public static class Node<T>{
         Node<T> next;
         T value;
         public Node(T value){
@@ -25,37 +25,106 @@ public class SinglyLinkedList<T> {
         return head;
     }
 
-    public void setHead(Node<T> head) {
-        this.head = head;
-    }
 
     public Node<T> getTail() {
         return tail;
     }
 
-    public void setTail(Node<T> tail) {
-        this.tail = tail;
+    public void add(T value){
+        Node<T> node = new Node<>(value);
+        if(this.head==null){
+            head = node;
+            tail = node;
+        } else {
+            // set tail.next to new node
+            tail.next = node;
+
+            // point tail to new node
+            tail = node;
+        }
     }
 
-    public void add(Node<T> node){
+    public void addNode(Node<T> node){
         if(this.head==null){
             head = node;
         } else {
-            Node<T> temp = head;
-            while(temp.next!=null){
-                temp = temp.next;
-            }
-            temp.next = node;
+            // set tail to new node
+            tail.next = node;
         }
+        tail = node;
     }
 
     public Integer size(){
         Integer count  = 0;
         Node<T> temp = head;
-        while(temp.next!=null){
+        while(temp!=null){
             count++;
             temp = temp.next;
         }
         return count;
     }
+
+    public Node<T> get(Integer index){
+        if(index < this.size()){
+            Integer count  = 0; // current index
+            Node<T> temp = head;
+            while(temp!=null){
+                if(count.equals(index)){
+                    return temp;
+                }
+                count++;
+                temp = temp.next;
+            }
+        }
+        return null;
+    }
+
+    public Boolean contains(Node<T> node){
+        Node<T> temp = head;
+        while(temp!=null){
+            if(temp.equals(node)){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public Integer find(T value){
+        Node<T> temp = head;
+        Integer index = 0;
+        while(temp!=null){
+            if(temp.value.equals(value)){
+                return index;
+            }
+            index++;
+            temp = temp.next;
+        }
+        return -1;
+    }
+
+    public Integer findNode(Node<T> node) {
+        Node<T> temp = head;
+        Integer index = 0;
+        while (temp != null) {
+            if (temp.equals(node)) {
+                return index;
+            }
+            index++;
+            temp = temp.next;
+        }
+        return -1;
+    }
+
+    public SinglyLinkedList<T> copy()  {
+        SinglyLinkedList<T> newList = new SinglyLinkedList<>();
+        Node<T> temp = head;
+        while(temp!=null){
+            newList.add(temp.value);
+            temp = temp.next;
+        }
+        return newList;
+    }
+
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -14,4 +14,228 @@ public class SinglyLinkedListTest {
         Assert.assertNull(list.getTail());
     }
 
+    @Test
+    public void testAddValue(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        Integer expected = 5;
+
+        // When
+        list.add(5);
+
+        // Then
+        Assert.assertEquals(expected,list.getHead().value);
+        Assert.assertEquals(expected,list.getTail().value);
+    }
+
+    @Test
+    public void testAddValue2(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        Integer expectedHead = 5;
+        Integer expectedTail = 3;
+
+        // When
+        list.add(5);
+        list.add(2);
+        list.add(3);
+
+        // Then
+        Assert.assertEquals(expectedHead,list.getHead().value);
+        Assert.assertEquals(expectedTail,list.getTail().value);
+    }
+
+    @Test
+    public void testAddNode(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        Integer expectedHead = 5;
+        Integer expectedTail = 3;
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        // Then
+        Assert.assertEquals(expectedHead,list.getHead().value);
+        Assert.assertEquals(expectedTail,list.getTail().value);
+    }
+
+    @Test
+    public void testSize(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        Integer expectedHead = 3;
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        // Then
+        Assert.assertEquals(expectedHead,list.size());
+    }
+
+    @Test
+    public void testGet(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        // Then
+        Assert.assertNull(list.get(3));
+    }
+
+    @Test
+    public void testGet2(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        SinglyLinkedList.Node<Integer> node = new SinglyLinkedList.Node<>(10);
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(node);
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        // Then
+        Assert.assertEquals(node,list.get(1));
+    }
+
+    @Test
+    public void testGet3(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        SinglyLinkedList.Node<Integer> node = new SinglyLinkedList.Node<>(10);
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+        list.addNode(node);
+
+        // Then
+        Assert.assertEquals(node,list.get(3));
+    }
+
+    @Test
+    public void testContainsTrue(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        SinglyLinkedList.Node<Integer> node = new SinglyLinkedList.Node<>(10);
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+        list.addNode(node);
+
+        // Then
+        Assert.assertTrue(list.contains(node));
+    }
+
+    @Test
+    public void testContainsFalse(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        SinglyLinkedList.Node<Integer> node = new SinglyLinkedList.Node<>(10);
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+        //list.addNode(node);
+
+        // Then
+        Assert.assertFalse(list.contains(node));
+    }
+
+    @Test
+    public void testFindValue(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        Integer expected = 2;
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        // Then
+        Assert.assertEquals(expected,list.find(3));
+    }
+
+    @Test
+    public void testFindValue2(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        Integer expected = -1;
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        // Then
+        Assert.assertEquals(expected,list.find(10));
+    }
+
+    @Test
+    public void testFindNode(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        SinglyLinkedList.Node<Integer> node = new SinglyLinkedList.Node<>(10);
+        Integer expected = 2;
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(node);
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        // Then
+        Assert.assertEquals(expected,list.findNode(node));
+    }
+
+    @Test
+    public void testFindNode2(){
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        SinglyLinkedList.Node<Integer> node = new SinglyLinkedList.Node<>(10);
+        Integer expected = -1;
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        //list.addNode(node);
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        // Then
+        Assert.assertEquals(expected,list.findNode(node));
+    }
+
+    @Test
+    public void copy() {
+        // Given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        Integer expected = 5;
+        Integer expectedNewList =4;
+
+        // When
+        list.addNode(new SinglyLinkedList.Node<>(5));
+        list.addNode(new SinglyLinkedList.Node<>(2));
+        list.addNode(new SinglyLinkedList.Node<>(3));
+
+        SinglyLinkedList<Integer> newList = list.copy();
+        newList.getHead().value = 4;
+
+
+        // Then
+        Assert.assertEquals(expected,list.getHead().value);
+        Assert.assertEquals(expectedNewList,newList.getHead().value);
+    }
+
 }
